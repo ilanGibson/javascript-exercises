5a29d6f4fd76f389af670d2055eb7112cfe0314f
ilanGibson <ilanjgibson@gmail.com>
2023-11-08 14:35:31 -0700

    fix: reverseString.js
    fix reverseString.js to pass all spec tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    temp = []
+    for (let i = s.length - 1; i >= 0; i--) {
+        temp.push(s[i]);
+    }
+    let final = temp.join('');
+    return (final);
 };
 
+// console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
