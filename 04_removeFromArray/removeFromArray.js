1e3ebbb21020ab2ab65af540a0cea93edc483b30
ilanGibson <ilanjgibson@gmail.com>
2023-11-08 16:07:10 -0700

    fix: removeFromArray.js
    fix removeFromArray.js to pass all spec tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+        console.log("array: ", array);
+        console.log("items to remove", itemsToRemove);
 
+        for (const index in itemsToRemove) {
+            if (array.includes(itemsToRemove[index])) {
+                indexToRemove = array.indexOf(itemsToRemove[index]);
+                array.splice(indexToRemove, 1);
+            }
+        }
+        return (array);
 };
+
+console.log(removeFromArray([1,2,3,4], 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
