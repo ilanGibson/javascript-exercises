ae4c49d24aa192d136f9ddae1c83ba68222cbcb9
ilanGibson <ilanjgibson@gmail.com>
2023-11-08 11:32:18 -0700

    fix: repeatString.js
    fix repeatString.js to pass all spec tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(s, num) {
 
+    if (num === 0) {
+        return ("")
+    } else if (num < 0) {
+        return ("ERROR")
+    }
+    let temp = []
+    for (let i = 0; i < num; i++) {
+        temp.push(s);
+    }
+    let final = temp.join('');
+    return (final);
 };
 
 // Do not edit below this line
