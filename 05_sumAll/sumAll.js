f54b90a3b49ca99ac520dbf637e845841d3a5e7c
ilanGibson <ilanjgibson@gmail.com>
2023-11-08 16:42:28 -0700

    fix: sumAll.js
    fix sumAll.js to pass all spec tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    // if ((isNaN(start)) || (isNaN(stop)) || (start < 1) || (stop < 1)) {
+    if ((typeof start !== 'number') || (typeof stop !== "number") || (start < 1) || (stop < 1)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (stop < start) {
+        let temp = stop;
+        stop = start;
+        start = temp;
+    }
+
+    for (let i = start; i <= stop; i++) {
+        sum += i;
+    }
+    return (sum);
 
 };
 
-// Do not edit below this line
+console.log(sumAll(10, "90"));
+// Do not edit below this line 7626
 module.exports = sumAll;
