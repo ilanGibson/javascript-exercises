59a9f9679e3e0175bd734a85c65336c45667f6cb
ilanGibson <ilanjgibson@gmail.com>
2023-11-09 11:01:24 -0700

    fix: getTheTitles.js
    fix getTheTitles.js to pass all spec tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const valuesArray = books.map(obj => obj.title);
+    return (valuesArray);
 };
 
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
