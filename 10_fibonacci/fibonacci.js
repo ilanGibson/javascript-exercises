5a7b213a7033bd34c100fa03a13d45af6575f5d3
ilanGibson <ilanjgibson@gmail.com>
2023-11-09 10:30:42 -0700

    fix: fibonacci.js
    fix fibonacci.js to pass all spec tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return ("OOPS")
+    }
+    let fib = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
 };
 
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
