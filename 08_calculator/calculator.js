d1e7ae4e74c7ebfcf97ce924ed6b94dc71a0b2be
ilanGibson <ilanjgibson@gmail.com>
2023-11-08 22:41:20 -0700

    fix: calculator.js
    fix calculator.js to pass all spec tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,49 @@
-const add = function() {
-	
+const add = function(number1, ...otherNumbers) {
+  let sum = 0;
+  sum += number1;
+  for (number of otherNumbers) {
+    sum += number;
+  }
+
+  return (sum);
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+  return (number1 - number2);
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+  let sum = 0;
+  for (number of numberArray) {
+    sum += number;
+  }
+
+  return (sum);
 };
 
-const multiply = function() {
+const multiply = function(numberArray) {
+  let product = 1;
+  for (number of numberArray) {
+    product *= number;
+  }
 
+  return (product);
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return (number1 ** number2);
 };
 
-const factorial = function() {
-	
+const factorial = function(number1) {
+  temp = 1
+	for (let i = number1; i > 0; i--) {
+    temp *= i;
+  }
+
+  return (temp);
 };
 
+console.log(power(3,2));
 // Do not edit below this line
 module.exports = {
   add,
