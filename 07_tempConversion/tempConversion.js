cee6af1fa680c3f33a4ccfe5ed421a9602344239
ilanGibson <ilanjgibson@gmail.com>
2023-11-08 17:26:36 -0700

    fix: tempConversion.js
    fix tempConversion.js to pass all spec tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftemp) {
+  let ctemp = ((ftemp - 32) * (5/9));
+  ctemp = Math.round(ctemp * 10) / 10;
+  return (ctemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctemp) {
+  let ftemp = ((ctemp * 9/5) + 32);
+  ftemp = Math.round(ftemp * 10) / 10;
+  return (ftemp);
 };
 
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
