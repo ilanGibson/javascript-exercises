272fd564f7cea3f1e2762f1e8e0d3291d0a3e4cf
ilanGibson <ilanjgibson@gmail.com>
2023-11-09 09:37:08 -0700

    fix: palindromes.js
    fix palindromes.js to pass all spec tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    s = s.toLowerCase();
+    let toReverse = [];
+    const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
+    for (item of s) {
+        if (punctuation.includes(item)) {
+            continue;
+        } else if (item === " ") {
+            continue;
+        }
+        toReverse.push(item);
+    }
+    toReverse.reverse();
+    let temp = toReverse.slice().reverse();
+    console.log(temp.join(""));
+    console.log(toReverse.join(""));
+    if (temp.join("") === toReverse.join("")) {
+        return (true);
+    } else {
+        return (false);
+    }
 };
 
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 // Do not edit below this line
 module.exports = palindromes;
